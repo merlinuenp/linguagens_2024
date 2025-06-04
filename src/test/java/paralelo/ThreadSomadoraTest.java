56f7d78abd49619fe482d04acbc75378c6fdb7b6
guaragames <guaralauncher@gmail.com>
2025-06-04 17:10:06 -0300

    Threads.

--- src/test/java/paralelo/ThreadSomadoraTest.java
+++ src/test/java/paralelo/ThreadSomadoraTest.java
@@ -23,19 +23,14 @@ public class ThreadSomadoraTest {
                                     .boxed().collect(Collectors.toList());
         ThreadSomadora t1 = new ThreadSomadora(lista.subList(0, lista.size() / 2));
         ThreadSomadora t2 = new ThreadSomadora(lista.subList(lista.size() / 2, lista.size()));
-        
-        
-        
-//        int resultado = t1.call() + t2.call();
-//        System.out.println("\n" + resultado + "\n");
-        
-        
+               
         ExecutorService pool = Executors.newFixedThreadPool(2);
         
         long inicio = System.currentTimeMillis();
+        
         Future<Integer> futuro1 = pool.submit(t1);
         Future<Integer> futuro2 = pool.submit(t2);
-        System.out.println("Quantidade: " + (futuro1.get() + futuro2.get()));
+        System.out.println("Soma: " + (futuro1.get() + futuro2.get()));
 
         System.out.println("Tempo: " + (System.currentTimeMillis() - inicio));
         pool.shutdown();
